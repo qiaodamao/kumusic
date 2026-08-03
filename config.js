8924bcf967f3857e365139ff97cd220bf254bdc0
qiaodamao <1@shijuefuhao.com>
2026-08-03 17:21:52 +0800

    提交

---
+++ config.js
@@ -0,0 +1,22 @@
+// Meting API 地址（部署在 Vercel 的 Node 版 meting-api）
+window.meting_api = 'https://meting-api-sage-eta.vercel.app/api?server=:server&type=:type&id=:id&r=:r';
+
+var userId = "8668419170";
+var userServer = "tencent";
+var userType = "playlist";
+// var localMusic = [{
+//     name: '重生之我在异乡为异客',
+//     artist: '王睿卓',
+//     url: '/music/重生之我在异乡为异客.mp3',
+//     cover: '/music/重生之我在异乡为异客.png',
+//     lrc: '/music/重生之我在异乡为异客.lrc'
+// },
+// {
+//     name: '落',
+//     artist: '唐伯虎',
+//     url: '/music/落.mp3',
+//     cover: '/music/落.png',
+//     lrc: '/music/落.lrc'
+// }
+// ];
+// var remoteMusic = "./musiclist.json"
