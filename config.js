f83e8450bb2003c2d380b9be931b301ce1fa2981
qiaodamao <1@shijuefuhao.com>
2026-08-03 17:42:11 +0800

    提交

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // Meting API 地址（部署在 Vercel 的 Node 版 meting-api）
 window.meting_api = 'https://meting-api-sage-eta.vercel.app/api?server=:server&type=:type&id=:id&r=:r';
 
-var userId = "2619366284";
+var userId = "21158650";
 var userServer = "netease";
 var userType = "playlist";
 // var localMusic = [{
